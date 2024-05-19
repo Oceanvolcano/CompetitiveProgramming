cd9a9dba1762db068d4f90ed0a5989ccfc00e253
OceanVolcano <134344815+Oceanvolcano@users.noreply.github.com>
2024-05-19 16:57:11 +0700

    Update

--- OnlineJudges/Codeforces/template.cpp
+++ OnlineJudges/Codeforces/template.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef string str;
+
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define nl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    
+}
