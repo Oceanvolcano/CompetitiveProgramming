13103564a0c2e55b2c3e1a1713256d9f946a0581
OceanVolcano <134344815+Oceanvolcano@users.noreply.github.com>
2024-05-23 09:32:08 +0700

    ok

--- OnlineJudges/CSES/Sorting-and-Searching/DistinctNumbers.cpp
+++ OnlineJudges/CSES/Sorting-and-Searching/DistinctNumbers.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+#define nl '\n'
+#define ll long long
+
+int main() {
+   ll n;
+   cin >> n;
+
+   vector<int> a(n);
+   for (int i = 0; i < n; ++i) {
+      cin >> a[i];
+   }
+
+   map<int, int> distinct;
+   for (int i = 0; i < n; ++i) {
+    distinct[a[i]]++;
+   }
+   cout << distinct.size() << nl;
+}
