ac9430567b268d61abe2b28ab2ce9a9698106ab8
OceanVolcano <134344815+Oceanvolcano@users.noreply.github.com>
2024-06-18 18:17:57 +0700

    o

---
+++ OnlineJudges/CSES/1_Introductory-Problems/MissingNumbers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define ll long long
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   ll n, a;
+   cin >> n;
+
+   ll sum = 0;
+   for (int i = 0; i < n-1; ++i) {
+      cin >> a;
+      sum += a;
+   }
+   cout << (n*(n+1)/2) - sum << nl;
+}
