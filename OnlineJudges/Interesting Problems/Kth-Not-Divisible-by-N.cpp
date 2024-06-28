73fc2eec1a6036ca742138c310f5b4d238458899
Muhammad Fathurahman <134344815+Oceanvolcano@users.noreply.github.com>
2024-06-28 11:06:26 +0700

    ok

---
+++ OnlineJudges/Interesting Problems/Kth-Not-Divisible-by-N.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+#define nl '\n'
+ 
+void solve() {
+   ll n, k;
+   cin >> n >> k;
+   
+   ll temp = (k-1)/(n-1);
+   cout << k+temp << nl;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+      solve();
+    }
+}
+
+//Formula = floor(k-1)/(n-1) + k
+//https://codeforces.com/problemset/problem/1352/C
+//Tags = Number Theory, Binary Search
