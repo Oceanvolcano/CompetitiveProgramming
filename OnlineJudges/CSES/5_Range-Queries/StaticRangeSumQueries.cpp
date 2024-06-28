db5083d2d79e5a4ee741a0634c542903e8296464
Muhammad Fathurahman <134344815+Oceanvolcano@users.noreply.github.com>
2024-06-28 11:02:12 +0700

    ok

--- OnlineJudges/CSES/5_Range-Queries/StaticRangeSumQueries.cpp
+++ OnlineJudges/CSES/5_Range-Queries/StaticRangeSumQueries.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+#define nl '\n'
+using ll = long long;
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   ll n, q;
+   cin >> n >> q;
+
+   vector<ll> x(n);
+   for (ll i = 0; i < n; ++i) {
+      cin >> x[i];
+   }
+
+   vector<ll> pref(n, 0);
+   for (int i = 0; i < n; ++i) {
+      pref[i+1] = pref[i] + x[i];
+   }
+
+   for (ll i = 0; i < q; ++i) {
+      ll a, b;
+      cin >> a >> b;
+      cout << pref[b] - pref[a-1] << nl;
+   }
+}
