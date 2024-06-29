5b0251bdb3417feb230ae2b32c3381a0b9c8ebc4
Muhammad Fathurahman <134344815+Oceanvolcano@users.noreply.github.com>
2024-06-29 09:18:06 +0700

    messy

--- OnlineJudges/CSES/2_Sorting-and-Searching/Ferris-Wheel.cpp
+++ OnlineJudges/CSES/2_Sorting-and-Searching/Ferris-Wheel.cpp
@@ -6,29 +6,29 @@ using namespace std;
 using ll = long long;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	ll n, x;
-	cin >> n >> x;
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   ll n, x;
+   cin >> n >> x;
 
    vector<int> p(n);
-	for (int i = 0; i < n; ++i) cin >> p[i];
-	sort(all(p));
+   for (int i = 0; i < n; ++i) cin >> p[i];
+   sort(all(p));
 
    vector<bool> naik(n, false);
    ll ans = 0;
-	ll l = 0, r = n - 1;
-	while (l < r) {
-		if (p[l] + p[r] > x) {
-			--r;
-		} else { 
-			++ans;
-			naik[l] = naik[r] = true;
-			++l; --r; 
-		}
-	}
-	for (int i = 0; i < n; ++i) {
-		ans += naik[i] == false;
-	}
-	cout << ans;
+   ll l = 0, r = n - 1;
+   while (l < r) {
+      if (p[l] + p[r] > x) {
+         --r;
+      } else { 
+         ++ans;
+         naik[l] = naik[r] = true;
+         ++l; --r; 
+      }
+   }
+   for (int i = 0; i < n; ++i) {
+      ans += naik[i] == false;
+   }
+   cout << ans;
 }
